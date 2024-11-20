c32d70a41e52139663d681ccf62549674842053a
phamtan220106 <tanpham220106z@gmail.com>
2024-11-20 21:33:32 +0700

    ex5

---
+++ ex5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("tinh s và chu vi hinh chu nhat\n");
+    float Dai,Rong;
+    printf("nhap Dai: \n");
+    scanf("%f",&Dai);
+    printf("nhap Rong: \n");
+    scanf("%f",&Rong);
+    float s=Dai*Rong;
+    float ChuVi=(Dai+Rong)*2;
+    printf("dien tich hcn la: %f\n",s);
+    printf("chu vi hcn la: %f\n",ChuVi);
+    printf("\n");
+    printf("pham_nhat_tan_s2_5\n");
+    printf("Hello world!\n");
+    return 0;
+}
