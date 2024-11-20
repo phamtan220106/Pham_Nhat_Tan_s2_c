5269f3e8eba799838434379c69b87b0319ceb810
phamtan220106 <tanpham220106z@gmail.com>
2024-11-20 21:33:58 +0700

    ex6

---
+++ ex6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("tinh chu vi va dien tich hinh tron\n");
+    float bankinh,pi=3.14;
+    printf("nhap ban kinh hinh tron: \n");
+    scanf("%f",&bankinh);
+    float s=pi*bankinh*bankinh;
+    float ChuVi=pi*2*bankinh;
+    printf("dien tich hinh tron la: %f\n",s);
+    printf("chu vi hinh tron la: %f\n",ChuVi);
+    printf("\n");
+    printf("pham_nhat_tan_s2_6\n");
+    printf("Hello world!\n");
+    return 0;
+}
