c75b5b6535995455a54f45ee66860ed3b1b06be4
phamtan220106 <tanpham220106z@gmail.com>
2024-11-20 21:32:44 +0700

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("tinh dien tich hinh vuong\n");
+    float canh;
+    scanf("%f",&canh);
+    float DienTich = canh*canh;
+    printf("dien tich hinh vuong la:%f\n",DienTich);
+    printf("\n");
+    printf("pham_nhat_tan_s2_ex4\n");
+    printf("Hello world!\n");
+    return 0;
+}
